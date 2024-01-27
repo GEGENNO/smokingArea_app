2746a7d34b63992b7054a08dcf03257196e48eb9
masaya-niigawa-private <mab29597@gmail.com>
2024-01-27 21:19:00 +0900

    初回コミット（2024/1/27）

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1 style="color: red">Hello World!</h1>
+  <p>{{ $name }}さんこんにちは</p>
+  <p>今は{{ $time }}時です。</p>
+</body>
+</html>
